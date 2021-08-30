fc1d1095da4ed6791b0212ff21733e8bf88957e1
borchard-dev <borchard.dev@gmail.com>
2021-08-30 14:22:44 +1000

    more exercises

---
+++ exercises02/countIndex/count.php
@@ -0,0 +1,8 @@
+<?php
+//This file is also known as index.php
+  $output = '';
+  for ($count = 1; $count <= 10; $count++) {
+    $output .= $count . ' ';
+  }
+  include 'countHTML/count.html.php';
+ ?>
---
+++ exercises02/countIndex/countHTML/count.html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!-This is a template file
+.html.php file extension is simply to say that there is also html in the file ->
+<html lang='en'>
+  <head>
+    <meta charset='utf-8'>
+    <title>Counting to ten</title>
+  </head>
+  <body>
+    <p>
+      <?php echo $output; ?>
+    </p>
+  </body>
+</html>
